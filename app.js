7ec4b84a1612bb6b1a293b0bf68abe7b9dfccdf2
lim <wamidulim@gmail.com>
2021-05-01 11:24:23 +0800

    fixed ui

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 //this is to copy the number to clipboard
 function myFunction() {
-    /* Get the text field */
-    var copyText = document.getElementById("cpnum"); 
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */ 
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
-    console.log(1);
-  } 
-  
-  document.getElementById("cpnum").addEventListener('click',myFunction); 
+  /* Get the text field */
+  var copyText = document.getElementById("cpnum");
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+  console.log(1);
+  document.querySelector('.fa-copy').textContent = 'copied';
+  setTimeout(function () { document.querySelector('.fa-copy').textContent = ''; }, 3000);
+}
+
+document.getElementById("cpnum").addEventListener('click', myFunction);
